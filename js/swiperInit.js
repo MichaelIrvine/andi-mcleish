89f63a11deb47009e3f59788c868e402e5602c69
Michael Irvine <michaelirvine@MacBook-Pro-2.local>
2019-08-19 11:47:54 -0700

    adding SCSS

---
+++ js/swiperInit.js
@@ -0,0 +1,44 @@
+// Global Var Pages
+const body = document.querySelector('body');
+
+
+if ( body.classList.contains('home') ) { 
+
+const fpSlider = function() {
+	
+	const fpSwiper = new Swiper('.swiper-container', {
+		direction: 'horizontal',
+		loop: false,
+		slidesPerView: 'auto',
+		spaceBetween: 30,
+		parallax: true,
+		freeMode: true,
+		freeModeSticky: false,
+		freeModeMomentum: true,
+		preventInteractionOnTransition: true,
+		// mousewheel: {
+		// 	enabled: true,
+		// 	releaseOnEdges: true,
+		// },
+		autoplay: false,
+		sensitivity: 1,
+		keyboard: {
+			enabled: true,
+		},
+		watchSlidesVisibility: true,
+		preloadImages: true,
+		updateOnImagesReady: true,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		  },
+	
+	});
+
+
+
+};
+
+fpSlider();
+};
