a9585e3eb895644637349cd9deaf78147c16c8a9
Michael Irvine <michaelirvine@MacBook-Pro-2.local>
2019-08-22 15:17:35 -0700

    still tryin to fix portfolio page

--- page-portfolio.php
+++ page-portfolio.php
@@ -14,26 +14,28 @@ get_header();
 <div id="portfolio-primary" class="portfolio_content-area">
     <main id="main_portfolio" class="portfolio-main">
         <?php
-				$tax_terms = get_terms('project_types');
-				?>
+			$tax_terms = get_terms('project_types');
+			$images_array = array();
+			var_dump($images_array);
+		?>
         <div class="project-list-container">
 			<ul class="portfolio-list-container">
 			<?php
 				foreach($tax_terms as $tax_term) : 
 				$term_link = get_term_link( $tax_term );
-				$term_image = get_field('project_type_feature_image',$tax_term);
+				$term_image = get_field('project_type_feature_image', $tax_term);
 				$size = 'full';
+				array_push($images_array, $term_image);
 				
 				?>
 
             
-                <li class="project-list-title"><a href="<?php echo $term_link; ?>"><?php echo $tax_term->name; ?></a></li>
+                <li class="project-list-title" data-portfolio-image="<?php echo $tax_term->name; ?>"><a href="<?php echo $term_link; ?>"><?php echo $tax_term->name; ?></a></li>
+			
+			
             <?php endforeach;?>
+			<img class="portfolio-img" data-portfolio-image="<?php echo $tax_term->name; ?>" src="<?php echo $term_image['url']; ?>" alt="<?php echo $term_image['alt']; ?>" />	
 			</ul> <!-- End of .portfolio-list-container -->
-
-            <!-- <ul class="portfolio-feature-image-container">
-                <li><img src="<?php echo $term_image['url']; ?>" alt="<?php echo $term_image['alt']; ?>" /></li>
-            </ul> -->
         </div> <!-- End Project List Container -->
 
     </main><!-- #main -->
