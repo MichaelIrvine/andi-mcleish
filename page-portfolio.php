72b83ed75735b69ea4e0cd750f78d69cf87a4fec
Michael Irvine <michaelirvine@MacBook-Pro-2.local>
2019-08-22 14:17:10 -0700

    Working out custom taxonomy images

--- page-portfolio.php
+++ page-portfolio.php
@@ -11,45 +11,33 @@
 get_header();
 ?>
 
-	<div id="portfolio-primary" class="portfolio_content-area">
-		<main id="main_portfolio" class="portfolio-main">
-		<?php
+<div id="portfolio-primary" class="portfolio_content-area">
+    <main id="main_portfolio" class="portfolio-main">
+        <?php
 				$tax_terms = get_terms('project_types');
 				?>
-				<div class="project-list-container">
-				<?php
+        <div class="project-list-container">
+			<ul class="portfolio-list-container">
+			<?php
 				foreach($tax_terms as $tax_term) : 
 				$term_link = get_term_link( $tax_term );
 				$term_image = get_field('project_type_feature_image',$tax_term);
+				$size = 'full';
 				
 				?>
-				
-				<h3 class="project-list-title"><a href="<?php echo $term_link; ?>"><?php echo $tax_term->name; ?></a></h3>
-				<?php
-				if( $image ) {
 
-					echo wp_get_attachment_image( $image, $size );
+            
+                <li class="project-list-title"><a href="<?php echo $term_link; ?>"><?php echo $tax_term->name; ?></a></li>
+            <?php endforeach;?>
+			</ul> <!-- End of .portfolio-list-container -->
 
-				}
-				$args = array('post_type' => 'projects',
-											'posts_per_page' => -1,
-											'hide_empty' => true,
-											'orderby' => 'term_order',											
-											'tax_query' => array( 
-												 	array(
-													'taxonomy' => 'projects',
-													'field'    => 'slug',
-													'terms'    => $tax_term->name,
-											)
-										)
-									);
+            <!-- <ul class="portfolio-feature-image-container">
+                <li><img src="<?php echo $term_image['url']; ?>" alt="<?php echo $term_image['alt']; ?>" /></li>
+            </ul> -->
+        </div> <!-- End Project List Container -->
 
-				
-				endforeach;?>
-				</div> <!-- End Project List Container -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_footer();
