3e6a31e1962f0940ee1b4d415de72dd0ced5afe4
Michael Irvine <michaelirvine@MacBook-Pro-2.local>
2019-10-08 12:18:50 -0700

    Getting close to pushing live site

--- js/featurePortfolioImages.js
+++ js/featurePortfolioImages.js
@@ -1,34 +1,24 @@
 export const featurePortfolioImages = (function() {
-
   // --Vars
   const portfolioLinks = document.querySelectorAll("li[data-portfolio-image]");
 
-  let viewPortSize = window.matchMedia('(max-width: 768px)');
+  portfolioLinks.forEach(link => {
+    link.addEventListener("mouseenter", function(e) {
+      let hoveredEl = link.dataset.portfolioImage;
 
-  
-    if(!viewPortSize){
-        portfolioLinks.forEach(link => {
-            link.addEventListener("mouseenter", function(e) {
-              let hoveredEl = link.dataset.portfolioImage;
-        
-              document
-                .querySelector(`img[data-portfolio-image=${hoveredEl}]`)
-                .classList.add("display-image");
-            });
-          });
-        
-          portfolioLinks.forEach(link => {
-            link.addEventListener("mouseleave", function(e) {
-              let hoveredEl = link.dataset.portfolioImage;
-        
-              document
-                .querySelector(`img[data-portfolio-image=${hoveredEl}]`)
-                .classList.remove("display-image");
-            });
-          });
-    };
-  
+      document
+        .querySelector(`img[data-portfolio-image=${hoveredEl}]`)
+        .classList.add("display-image");
+    });
+  });
 
+  portfolioLinks.forEach(link => {
+    link.addEventListener("mouseleave", function(e) {
+      let hoveredEl = link.dataset.portfolioImage;
 
-
+      document
+        .querySelector(`img[data-portfolio-image=${hoveredEl}]`)
+        .classList.remove("display-image");
+    });
+  });
 })();
